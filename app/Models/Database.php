b57314938fcb7ee5984b8fed7501b0e50b3ecf5a
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-06-22 00:19:28 +0200

    Rebuild Database model class

--- app/Models/Database.php
+++ app/Models/Database.php
@@ -7,28 +7,22 @@ use mysqli;
 
 class Database
 {
-    private $host;
-    private $user;
-    private $password;
-    private $database;
+    private $connection;
 
     public function __construct()
     {
         $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
         $dotenv->load();
 
-        $this->host = $_ENV['DB_HOST'];
-        $this->user = $_ENV['DB_USER'];
-        $this->password = $_ENV['DB_PASSWORD'];
-        $this->database = $_ENV['DB_DATABASE'];
+        $this->connection = new mysqli(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'));
+
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+        }
     }
 
-    public function connectDB()
+    public function getConnection()
     {
-        $conn = new mysqli($this->host, $this->user, $this->password, $this->database);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        return $conn;
+        return $this->connection;
     }
 }
