d270a95a5e4204fb0c75ee0741ab0f03b5567197
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-06-22 01:23:43 +0200

    Added CommentController with included logging, with user ID.

---
+++ app/Controllers/CommentController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Comment;
+use App\Models\Log;
+
+class CommentController
+{
+    protected $commentModel;
+    protected $logger;
+
+    public function __construct()
+    {
+        $this->commentModel = new Comment();
+        $this->logger = new Log();
+    }
+
+    public function createComment($content, $postID, $userID, $date)
+    {
+        $this->commentModel->createComment($content, $postID, $userID, $date);
+        $this->logger->createLog('Comment created', time());
+    }
+
+    public function deleteComment($commentID)
+    {
+        $this->commentModel->deleteComment($commentID);
+        $this->logger->createLog('Comment deleted', time());
+    }
+
+    public function getCommentsByPostID($postID)
+    {
+        return $this->commentModel->getCommentsByPostID($postID);
+    }
+}
