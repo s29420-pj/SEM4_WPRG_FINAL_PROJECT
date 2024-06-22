8e4b61ad9cad78f03b51b0f709a31c27ce8c4b2f
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-06-22 02:59:28 +0200

    Update Database.php to use Symfony Dotenv

--- app/Models/Database.php
+++ app/Models/Database.php
@@ -2,27 +2,24 @@
 
 namespace App\Models;
 
-use Dotenv\Dotenv;
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
 use mysqli;
 
 class Database
 {
-    private $connection;
-
-    public function __construct()
-    {
-        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-        $dotenv->load();
-
-        $this->connection = new mysqli(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'));
-
-        if ($this->connection->connect_error) {
-            die("Connection failed: " . $this->connection->connect_error);
-        }
-    }
-
     public function getConnection()
     {
-        return $this->connection;
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__ . '/../../.env');
+
+        $connection = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
+
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+
+        return $connection;
     }
 }
