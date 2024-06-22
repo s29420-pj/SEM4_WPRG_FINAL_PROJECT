dc7ce7f9ca85646611723020572bb87ef79ebb63
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-06-22 20:52:26 +0200

    Added createComment.php for creating comments.

---
+++ public/createComment.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Controllers\CommentController;
+use App\Controllers\UserController;
+
+require_once '../vendor/autoload.php';
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $content = $_POST['content'];
+    $postId = $_POST['post_id'];
+
+    // Assuming you have a UserController and a method to get the logged in user's ID
+    $userController = new UserController();
+    $userId = $userController->getUserID();
+
+    if (empty($content)) {
+        header("Location: post.php?id=$postId&error=empty content");
+        exit();
+    }
+
+    $commentController = new CommentController();
+    $commentController->createComment($content, $postId, $userId);
+
+    header("Location: post.php?id=$postId");
+} else {
+    header("Location: index.php");
+}
